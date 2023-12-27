07c9f20763b988d34e7de4ccb30db3c401c3bca3
DeerajAlagarsamy <deerajalagarsamyol7xl9@gmail.com>
2023-12-27 14:24:54 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,7 @@ function Frndservice({ onSelectFrndPercent }) {
       <span>
         <select onChange={(e) => onSelectFrndPercent(e.target.value)}>
           <option value="0">Dissatisfied (0%)</option>
+
           <option value="5">It was okay (5%)</option>
           <option value="10">It was good (10%)</option>
           <option value="20">Absolutly amazing! (20%)</option>
